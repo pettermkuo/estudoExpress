a38916839d1eb55cf0e244cf468a39b11557876e
pettermkuo <petterm0023@gmail.com>
2021-03-31 10:36:09 -0300

    att

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require('express');
-
 const app = express();
 
-console.log(app);
+/*
+GET: buscar informação do back-end
+POST: criar informações do back-end
+PUT/PATCH: alterar informações do back-end
+DELETE: deletar informações do back-end
+*/
+//console.log(app);
+
+app.get('/', (request, response) => {
+    return response.json([
+        'projeto1',
+        'projeto2',
+    ]);
+})
+
+//app.post()
+
+app.listen(3000, () => {
+    console.log('Servidor rodando...');
+})
